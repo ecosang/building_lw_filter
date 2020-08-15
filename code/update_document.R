ead09bf8181d45c788176fe775db8ea73d5de2a6
ecosang <ecosang@gmail.com>
2020-08-15 15:07:38 +0900

    update notation

--- code/update_document.R
+++ code/update_document.R
@@ -4,3 +4,4 @@ rmarkdown::render(input='code/liu_west_filter_for_a_building_gray_box_model.Rmd'
 file.rename(from ="code/liu_west_filter_for_a_building_gray_box_model.html" , to ="code/filter.html")
 file.copy(from="code/filter.html",to="docs/filter.html",overwrite =TRUE)
 file.remove('code/filter.html')
+
